e3edf9bf8c6ac8b2bc0849b570c9815d5d9a6f90
mithatural <mithatutr473@gmail.com>
2024-10-06 22:40:25 +0300

    1:09:35

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from './ProjectStyles.module.css';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className='sectionTitle'>Projects</h1>
+      <div className={styles.projectsContainer}>
+        <a href=""></a>
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
