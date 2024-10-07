7ab3b5011dd24c0488063c580271dfc96963e530
mithatural <mithatutr473@gmail.com>
2024-10-07 12:38:03 +0300

    projectcard

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import styles from './ProjectStyles.module.css';
+import styles from "./ProjectsStyles.module.css";
+import viberr from "../../assets/assets/viberr.png";
+import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
-      <h1 className='sectionTitle'>Projects</h1>
+      <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <a href=""></a>
+        <ProjectCard src={viberr} link='https://github.com/mithatural/JS-Gamb' h3='Bet Machine' p={"Gambling Background"} />
       </div>
     </section>
   );
