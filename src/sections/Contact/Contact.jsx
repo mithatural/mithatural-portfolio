d3cd8334ee5dde6baca80db50d821b181d295a28
mithatural <mithatutr473@gmail.com>
2024-10-09 00:01:44 +0300

    Project Completely Finished

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <section id='contact' className={styles.container}>
     <h1 className='sectionTitle'>Contact</h1>
-    <form action="">
+    <form action="https://formspree.io/f/xkgnwyje" method='POST' >
         <div className='fromGroup'>
             <label htmlFor="name" hidden>
                 Name
@@ -16,7 +16,7 @@ function Contact() {
             <label htmlFor="email" hidden>
                 Email
             </label>
-            <input type="text" name='email' id='email' placeholder='Email' required />
+            <input type="email" name='email' id='email' placeholder='Email' required />
         </div>
 
         <div className='fromGroup'>
